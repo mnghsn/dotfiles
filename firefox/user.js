29a343c866ffc8ef1a3df7c8b9ac32ebfc23b84d
Jimmy Lin <jmlntw@gmail.com>
2016-03-13 12:12:55 +0800

    Disable SPDY in Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -86,6 +86,10 @@ user_pref("dom.disable_window_flip", true);
 // Disallow JavaScript to move or resize existing windows
 user_pref("dom.disable_window_move_resize", true);
 
+// Disable SPDY (SPDY would be deprecated in 2016)
+user_pref("network.http.spdy.enabled", false);
+user_pref("network.http.spdy.enabled.v3-1", false);
+
 // -----------------------------------------------------------------------------
 // Privacy
 // -----------------------------------------------------------------------------
