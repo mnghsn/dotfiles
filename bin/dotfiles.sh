5748fc45abfe3bfb2e7840ead7e86933a786223b
Jimmy Lin <jmlntw@gmail.com>
2016-03-23 00:20:12 +0800

    Add dotfiles installation script

---
+++ bin/dotfiles.sh
@@ -0,0 +1,227 @@
+#!/usr/bin/env bash
+#
+# Installs or updates dotfiles.
+
+# NOTE: If you want to fork this repository and maintain your own dotfiles, you
+# must substitute the following user name and repository name for your own.
+DOTFILES_GITHUB_USER="jmlntw"
+DOTFILES_GITHUB_REPO="dotfiles"
+
+DOTFILES_DIRECTORY="${HOME}/.dotfiles"
+DOTFILES_TARBALL_URL="https://github.com/${DOTFILES_GITHUB_USER}/${DOTFILES_GITHUB_REPO}/tarball/master"
+DOTFILES_GIT_REMOTE="https://github.com/${DOTFILES_GITHUB_USER}/${DOTFILES_GITHUB_REPO}.git"
+
+########################################
+# Prints help message.
+########################################
+run_help() {
+cat <<EOT
+Usage: $(basename "$0") [options]
+Options:
+  -h, --help        Print this help text.
+  --no-sync         Suppress pulling from the remote repository.
+EOT
+}
+
+########################################
+# Links or copies dotfiles.
+########################################
+sync_dotfiles() {
+  e_info "Setting up bash..."
+  link_file "bash/.bashrc"       "${HOME}/.bashrc"
+  link_file "bash/.bash_profile" "${HOME}/.bash_profile"
+
+  e_info "Setting up git..."
+  link_file "git/.gitconfig" "${HOME}/.gitconfig"
+  link_file "git/.gitignore" "${HOME}/.gitignore"
+
+  e_info "Setting up fontconfig..."
+  link_file "fontconfig" "${HOME}/.config/fontconfig"
+
+  e_info "Setting up vim..."
+  link_file "vim"        "${HOME}/.vim"
+  link_file "vim/.vimrc" "${HOME}/.vimrc"
+
+  e_info "Setting up Atom..."
+  link_file "atom" "${HOME}/.atom"
+
+  e_info "Setting up Firefox..."
+  for profile in $(find "${HOME}/.mozilla/firefox" -name "*.default" -type d); do
+    link_file "firefox" "${profile}"
+  done
+}
+
+########################################
+# Prints a normal message.
+########################################
+e_log() {
+  printf "$(tput sgr0)%s$(tput sgr0)\n" "$@"
+}
+
+########################################
+# Prints a information message.
+########################################
+e_info() {
+  printf "$(tput setaf 6)%s$(tput sgr0)\n" "$@"
+}
+
+########################################
+# Prints a success message.
+########################################
+e_success() {
+  printf "$(tput setaf 2)✓ %s$(tput sgr0)\n" "$@"
+}
+
+########################################
+# Prints a error message.
+########################################
+e_error() {
+  printf "$(tput setaf 1)✗ %s$(tput sgr0)\n" "$@"
+}
+
+########################################
+# Prints a warning message.
+########################################
+e_warning() {
+  printf "$(tput setaf 3)❢ %s $(tput sgr0)\n" "$@"
+}
+
+########################################
+# Asks user for confirmation before proceeding.
+########################################
+e_confirm() {
+  e_log "$@"
+  read -p "Do you want to continue? [y/N] " -n 1 </dev/tty
+  printf "\n"
+}
+
+########################################
+# Test whether the result of `e_confirm` is confirmed.
+########################################
+is_confirmed() {
+  if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    return 0
+  fi
+  return 1
+}
+
+########################################
+# Tests whether a command is exists.
+########################################
+type_exists() {
+  if [[ $(type -P $1) ]]; then
+    return 0
+  fi
+  return 1
+}
+
+########################################
+# Tests whether we are in a git repository.
+########################################
+is_git_repo() {
+  $(git rev-parse --is-inside-work-tree &> /dev/null)
+}
+
+########################################
+# Tests whether the previous command succeeded.
+########################################
+is_success() {
+  if [[ $? -eq 0 ]]; then
+    return 0
+  fi
+  return 1
+}
+
+########################################
+# Creates or replaces a symlink.
+########################################
+link_file() {
+  [[ -d "${2}" ]] && rm -rf "${2}"
+  ln -fs "${DOTFILES_DIRECTORY}/${1}" "${2}"
+  if is_success; then
+    e_success "Linked ${1} to ${2}"
+  else
+    e_error "Failed to link ${1} to ${2}"
+  fi
+}
+
+########################################
+# Copies file to specified folder.
+########################################
+copy_file() {
+  cp -f "${DOTFILES_DIRECTORY}/${1}" "${2}"
+  if is_success; then
+    e_success "Copied ${1} to ${2}"
+  else
+    e_error "Faild to copy ${1} to ${2}"
+  fi
+}
+
+
+# Print help text if `-h` or `--help` flag is specified.
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  run_help
+  exit 0
+fi
+
+# Test for all known flags.
+for opt in $@; do
+  case $opt in
+    --no-sync) NO_SYNC=true ;;
+    -*|--*) e_warning "Invalid option: $opt"
+  esac
+done
+
+# Check if cURL is installed.
+if ! type_exists "curl"; then
+  e_error "cURL must be installed first."
+  exit 1
+fi
+
+# Check if git is installed.
+if ! type_exists "git"; then
+  e_error "git must be installed first."
+  exit 1
+fi
+
+# Download and extract dotfiles if missing.
+if [[ ! -d "${DOTFILES_DIRECTORY}" ]]; then
+  e_info "Downloading dotfiles..."
+  mkdir -p "${DOTFILES_DIRECTORY}"
+  curl -fsSLo "${HOME}/dotfiles.tar.gz" ${DOTFILES_TARBALL_URL}
+  tar -zxf "${HOME}/dotfiles.tar.gz" --strip-components 1 -C "${DOTFILES_DIRECTORY}"
+  rm -rf "${HOME}/dotfiles.tar.gz"
+fi
+
+e_info "dotfiles directory is ${DOTFILES_DIRECTORY}"
+cd "${DOTFILES_DIRECTORY}"
+
+# Initialize dotfiles git repository if missing.
+if ! is_git_repo; then
+  e_info "Initializing dotfiles git repository..."
+  git init
+  git remote add origin ${DOTFILES_GIT_REMOTE}
+  git fetch origin master
+  git reset --hard FETCH_HEAD
+  git clean -fd
+fi
+
+# Conditionally sync with the remote repository.
+if [[ $NO_SYNC ]]; then
+  e_info "Skipped dotfiles sync."
+else
+  e_info "Syncing dotfiles..."
+  git pull --rebase origin master
+  git submodule update --recursive --init --quiet
+fi
+
+# Ask before potentially overwriting files.
+e_confirm "This step may overwrite your existing dotfiles."
+if is_confirmed; then
+  sync_dotfiles
+  source "${HOME}/.bash_profile"
+  e_success "Done. :)"
+else
+  e_warning "Aborted."
+  exit 1
+fi
