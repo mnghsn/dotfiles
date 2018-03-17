db2aadf67517c9c83256e5dec0e720aa37c46f1b
Jimmy Lin <jmlntw@gmail.com>
2018-03-17 22:35:32 +0800

    Add missing period

--- firefox/default/user.js
+++ firefox/default/user.js
@@ -4,7 +4,7 @@
 // Startup
 // -----------------------------------------------------------------------------
 
-// Show a blank page when Firefox starts
+// Show a blank page when Firefox starts.
 user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.startup.homepage_override.mstone", "ignore");
 user_pref("browser.startup.page", 0);
@@ -14,7 +14,7 @@ user_pref("startup.homepage_override_url", "");
 user_pref("browser.rights.3.shown", true);
 user_pref("browser.laterrun.enabled", false);
 
-// Show a blank page when opening new tab
+// Show a blank page when opening new tab.
 user_pref("browser.newtab.preload", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.newtabpage.enhanced", false);
@@ -23,7 +23,7 @@ user_pref("browser.newtabpage.directory.source", "data:text/plain,");
 user_pref("browser.newtabpage.activity-stream.enabled", false);
 user_pref("browser.library.activity-stream.enabled", false);
 
-// Disable Slow Startup notifications
+// Disable Slow Startup notifications.
 user_pref("browser.slowStartup.notificationDisabled", true);
 user_pref("browser.slowStartup.samples", 0);
 user_pref("browser.slowStartup.maxSamples", 0);
@@ -32,15 +32,15 @@ user_pref("browser.slowStartup.maxSamples", 0);
 // Localization
 // -----------------------------------------------------------------------------
 
-// Choose preferred languages for displaying pages
+// Choose preferred languages for displaying pages.
 user_pref("intl.accept_languages", "ja, zh-tw, zh, en-us, en");
 
-// Disable GeoIP-based search results
+// Disable GeoIP-based search results.
 user_pref("browser.search.countryCode", "JP");
 user_pref("browser.search.region", "JP");
 user_pref("browser.search.geoip.url", "");
 
-// Disable geographically specific results/search engines
+// Disable geographically specific results/search engines.
 user_pref("browser.search.geoSpecificDefaults", false);
 user_pref("browser.search.geoSpecificDefaults.url", "");
 
@@ -48,59 +48,59 @@ user_pref("browser.search.geoSpecificDefaults.url", "");
 // Content
 // -----------------------------------------------------------------------------
 
-// Set minimum font size
+// Set minimum font size.
 user_pref("font.minimum-size.ja", 10);
 user_pref("font.minimum-size.ko", 10);
 user_pref("font.minimum-size.zh-CN", 10);
 user_pref("font.minimum-size.zh-HK", 10);
 user_pref("font.minimum-size.zh-TW", 10);
 
-// Disable Beacon
+// Disable Beacon.
 user_pref("beacon.enabled", false);
 
-// Disable pings
+// Disable pings.
 user_pref("browser.send_pings", false);
 user_pref("browser.send_pings.require_same_host", true);
 
-// Disable camera image capture
+// Disable camera image capture.
 user_pref("dom.imagecapture.enabled", false);
 
-// Disable canvas capture stream
+// Disable canvas capture stream.
 user_pref("canvas.capturestream.enabled", false);
 
-// Disallow JavaScript to move or resize window
+// Disallow JavaScript to move or resize window.
 user_pref("dom.disable_window_move_resize", true);
 
-// Limit events that can cause a popup
+// Limit events that can cause a popup.
 user_pref("dom.popup_allowed_events", "click dblclick mouseup");
 
-// Decrease the maximum number of popups from a single non-click event
+// Decrease the maximum number of popups from a single non-click event.
 user_pref("dom.popup_maximum", 2);
 
-// Disable links opening in a new window
+// Disable links opening in a new window.
 user_pref("browser.link.open_newwindow.restriction", 0);
 
-// Disable Notifications API
+// Disable Notifications API.
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.webnotifications.serviceworker.enabled", false);
 user_pref("permissions.default.desktop-notification", 2);
 
-// Disable Push API
+// Disable Push API.
 user_pref("dom.push.enabled", false);
 user_pref("dom.push.connection.enabled", false);
 user_pref("dom.push.serverURL", "");
 user_pref("dom.push.userAgentID", "");
 
-// Disable Geolocation
+// Disable Geolocation.
 user_pref("geo.enabled", false);
 user_pref("geo.wifi.uri", "");
 user_pref("geo.wifi.logging.enabled", false);
 user_pref("permissions.default.geo", 2);
 
-// Disable audio auto-play in non-active tabs
+// Disable audio auto-play in non-active tabs.
 user_pref("media.block-autoplay-until-in-foreground", true);
 
-// Disable WebRTC
+// Disable WebRTC.
 user_pref("media.peerconnection.enabled", false);
 user_pref("media.peerconnection.ice.tcp", false);
 user_pref("media.peerconnection.identity.enabled", false);
@@ -111,16 +111,16 @@ user_pref("media.peerconnection.video.enabled", false);
 user_pref("media.navigator.enabled", false);
 user_pref("media.navigator.video.enabled", false);
 
-// Limit WebRTC IP leaks
+// Limit WebRTC IP leaks.
 user_pref("media.peerconnection.ice.default_address_only", true);
 user_pref("media.peerconnection.ice.no_host", true);
 
-// Disable WebRTC screen sharing
+// Disable WebRTC screen sharing.
 user_pref("media.getusermedia.screensharing.enabled", false);
 user_pref("media.getusermedia.audiocapture.enabled", false);
 user_pref("media.getusermedia.browser.enabled", false);
 
-// Disable WebGL
+// Disable WebGL.
 user_pref("webgl.disabled", true);
 user_pref("webgl.disable-extensions", true);
 user_pref("webgl.disable-fail-if-major-performance-caveat", true);
@@ -138,32 +138,32 @@ user_pref("permissions.default.microphone", 2);
 // Privacy
 // -----------------------------------------------------------------------------
 
-// Enable DNT HTTP header
+// Enable DNT HTTP header.
 user_pref("privacy.donottrackheader.enabled", true);
 
-// Enable Tracking Protection
+// Enable Tracking Protection.
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.pbmode.enabled", true);
 user_pref("privacy.trackingprotection.ui.enabled", true);
 user_pref("privacy.trackingprotection.introCount", 20);
 
-// Enable First Party Isolation
+// Enable First Party Isolation.
 user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.firstparty.isolate.restrict_opener_access", true);
 
-// Never accept third-party cookies
+// Never accept third-party cookies.
 user_pref("network.cookie.cookieBehavior", 1);
 
-// Disable prefetching
+// Disable prefetching.
 user_pref("network.prefetch-next", false);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.dns.disablePrefetchFromHTTPS", true);
 user_pref("network.predictor.enable-prefetch", false);
 
-// Disable opening connection to links when mouseover
+// Disable opening connection to links when mouseover.
 user_pref("network.http.speculative-parallel-limit", 0);
 
-// Disable Seer/Necko
+// Disable Seer/Necko.
 user_pref("network.predictor.enabled", false);
 user_pref("network.captive-portal-service.enabled", false);
 user_pref("captivedetect.canonicalURL", "");
@@ -172,14 +172,14 @@ user_pref("captivedetect.canonicalURL", "");
 // Security
 // -----------------------------------------------------------------------------
 
-// Enable Safe Browsing
+// Enable Safe Browsing.
 user_pref("browser.safebrowsing.phishing.enabled", true);
 user_pref("browser.safebrowsing.malware.enabled", true);
 
-// Disable saving passwords
+// Disable saving passwords.
 user_pref("signon.rememberSignons", false);
 
-// Enable OCSP stapling
+// Enable OCSP stapling.
 user_pref("security.ssl.enable_ocsp_stapling", true);
 user_pref("security.OCSP.enabled", 1);
 user_pref("security.OCSP.require", true);
@@ -188,18 +188,18 @@ user_pref("security.OCSP.require", true);
 // Features and Components
 // -----------------------------------------------------------------------------
 
-// Disable Health Report
+// Disable Health Report.
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
 
-// Disable Crash Reporter
+// Disable Crash Reporter.
 user_pref("breakpad.reportURL", "");
 user_pref("browser.tabs.crashReporting.sendReport", false);
 user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false);
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 
-// Disable Telemetry
+// Disable Telemetry.
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.archive.enabled", false);
@@ -212,172 +212,172 @@ user_pref("toolkit.telemetry.bhrPing.enabled", false);
 user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
 user_pref("toolkit.telemetry.hybridContent.enabled", false);
 
-// Disable Telemetry Experiments
+// Disable Telemetry Experiments.
 user_pref("experiments.enabled", false);
 user_pref("experiments.supported", false);
 user_pref("experiments.activeExperiment", false);
 user_pref("experiments.manifest.uri", "");
 user_pref("network.allow-experiments", false);
 
-// Disable Onboarding
+// Disable Onboarding.
 user_pref("browser.onboarding.enabled", false);
 
-// Disable Ping-centre telemetry
+// Disable Ping-centre telemetry.
 user_pref("browser.ping-centre.telemetry", false);
 
-// Disable UITour backend
+// Disable UITour backend.
 user_pref("browser.uitour.enabled", false);
 user_pref("browser.uitour.url", "");
 
-// Disable remote debugging
+// Disable remote debugging.
 user_pref("devtools.webide.enabled", false);
 user_pref("devtools.webide.autoinstallADBHelper", false);
 user_pref("devtools.debugger.force-local", true);
 user_pref("devtools.debugger.remote-enabled", false);
 user_pref("devtools.chrome.enabled", false);
 
-// Disable Reader View
+// Disable Reader View.
 user_pref("reader.parse-on-load.enabled", false);
 
 // -----------------------------------------------------------------------------
 // Add-ons and Plugins
 // -----------------------------------------------------------------------------
 
-// Disable extension metadata updating
+// Disable extension metadata updating.
 user_pref("extensions.getAddons.cache.enabled", false);
 
-// Disable Personas (themes) updating
+// Disable Personas (themes) updating.
 user_pref("lightweightThemes.update.enabled", false);
 
-// Disable "Get Add-ons" pane
+// Disable "Get Add-ons" pane.
 user_pref("extensions.getAddons.showPane", false);
 user_pref("extensions.webservice.discoverURL", "");
 
-// Lock down allowed extension directories
+// Lock down allowed extension directories.
 user_pref("extensions.enabledScopes", 1);
 
-// Clear localStorage and UUID when an extension is uninstalled
+// Clear localStorage and UUID when an extension is uninstalled.
 user_pref("extensions.webextensions.keepStorageOnUninstall", false);
 user_pref("extensions.webextensions.keepUuidOnUninstall", false);
 
-// Disable Form Autofill
+// Disable Form Autofill.
 user_pref("extensions.formautofill.available", "off");
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 user_pref("extensions.formautofill.heuristics.enabled", false);
 
-// Disable Pocket
+// Disable Pocket.
 user_pref("extensions.pocket.enabled", false);
 
-// Disable Shield
+// Disable Shield.
 user_pref("extensions.shield-recipe-client.enabled", false);
 user_pref("extensions.shield-recipe-client.api_url", "");
 
-// Disable Web Compatibility Reporter
+// Disable Web Compatibility Reporter.
 user_pref("extensions.webcompat-reporter.enabled", false);
 
-// Disable all plugins by default
+// Disable all plugins by default.
 user_pref("plugin.default.state", 0);
 user_pref("plugin.defaultXpi.state", 0);
 
-// Enable click-to-play and set the session permission
+// Enable click-to-play and set the session permission.
 user_pref("plugins.click_to_play", true);
 user_pref("plugin.sessionPermissionNow.intervalInMinutes", 0);
 
-// Disable scanning plugins on Windows
+// Disable scanning plugins on Windows.
 user_pref("plugin.scan.plid.all", false);
 
-// Disable all GMP (Gecko Media Plugins)
+// Disable all GMP (Gecko Media Plugins).
 user_pref("media.gmp-provider.enabled", false);
 user_pref("media.gmp-manager.url", "data:text/plain,");
 user_pref("media.gmp-manager.url.override", "data:text/plain,");
 user_pref("media.gmp-manager.updateEnabled", false);
 user_pref("media.gmp.trial-create.enabled", false);
 
-// Disable widevine CDM
+// Disable widevine CDM.
 user_pref("media.gmp-widevinecdm.visible", false);
 user_pref("media.gmp-widevinecdm.enabled", false);
 user_pref("media.gmp-widevinecdm.autoupdate", false);
 
-// Disable all DRM content
+// Disable all DRM content.
 user_pref("media.eme.enabled", false);
 user_pref("browser.eme.ui.enabled", false);
 
-// Disable OpenH264 Video Codec by Cisco
+// Disable OpenH264 Video Codec by Cisco.
 user_pref("media.gmp-gmpopenh264.enabled", false);
 user_pref("media.gmp-gmpopenh264.autoupdate", false);
 
-// Skip the installation delay
+// Skip the installation delay.
 user_pref("security.dialog_enable_delay", 0);
 
 // -----------------------------------------------------------------------------
 // Others
 // -----------------------------------------------------------------------------
 
-// Prevent accessibility services from accessing browser
+// Prevent accessibility services from accessing browser.
 user_pref("accessibility.force_disabled", 1);
 
-// Always ask me where to save download files
+// Always ask me where to save download files.
 user_pref("browser.download.useDownloadDir", false);
 
-// Disable downloading on desktop
+// Disable downloading on desktop.
 user_pref("browser.download.folderList", 2);
 
-// Always display download button on toolbar
+// Always display download button on toolbar.
 user_pref("browser.download.autohideButton", false);
 
-// Disable URL bar domain guessing
+// Disable URL bar domain guessing.
 user_pref("browser.fixup.alternate.enabled", false);
 
-// Strip password from URL
+// Strip password from URL.
 user_pref("browser.fixup.hide_user_pass", true);
 
-// Disable capturing page thumbnails
+// Disable capturing page thumbnails.
 user_pref("browser.pagethumbnails.capturing_disabled", false);
 
-// Display search bar by default
+// Display search bar by default.
 user_pref("browser.search.widget.inNavBar", true);
 
-// Disable closing browser with last tab
+// Disable closing browser with last tab.
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 
-// Disable Windows Taskbar Jump List
+// Disable Windows Taskbar Jump List.
 user_pref("browser.taskbar.lists.frequent.enabled", false);
 user_pref("browser.taskbar.lists.recent.enabled", false);
 
-// Disable URL bar autofill
+// Disable URL bar autofill.
 user_pref("browser.urlbar.autoFill", false);
 user_pref("browser.urlbar.autoFill.typed", false);
 
-// Disable search suggestions in the URL bar
+// Disable search suggestions in the URL bar.
 user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.urlbar.userMadeSearchSuggestionsChoice", true);
 user_pref("browser.urlbar.oneOffSearches", false);
 
-// Disable URL bar making speculative connections
+// Disable URL bar making speculative connections.
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 
-// Display all parts of the URL in the URL bar
+// Display all parts of the URL in the URL bar.
 user_pref("browser.urlbar.trimURLs", false);
 
-// Disable preloading top websites in the URL bar
+// Disable preloading top websites in the URL bar.
 user_pref("browser.urlbar.usepreloadedtopurls.enabled", false);
 
-// Disable site specific zoom
+// Disable site specific zoom.
 user_pref("browser.zoom.siteSpecific", false);
 
-// Disable `about:config` warnings
+// Disable `about:config` warnings.
 user_pref("general.warnOnAboutConfig", false);
 
-// Display the raw Punycode in the URL bar
+// Display the raw Punycode in the URL bar.
 user_pref("network.IDN_show_punycode", true);
 
-// Disable links launching Windows Store
+// Disable links launching Windows Store.
 user_pref("network.protocol-handler.external.ms-windows-store", false);
 
-// Disable Windows Family Safety Mode
+// Disable Windows Family Safety Mode.
 user_pref("security.family_safety.mode", 0);
 
-// Disable UI animation
+// Disable UI animation.
 user_pref("toolkit.cosmeticAnimations.enabled", false);
 user_pref("browser.stopReloadAnimation.enabled", false);
